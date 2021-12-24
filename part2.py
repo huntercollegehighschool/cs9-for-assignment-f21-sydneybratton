f3e2be46a6877017178a922ffaa66d82cb900dcf
Sydney Bratton <94643899+sydneybratton@users.noreply.github.com>
2021-12-24 23:29:32 +0000

    Part 1

--- part2.py
+++ part2.py
@@ -8,3 +8,8 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+number = int(input("Enter a positive integer: "))
+total = 0
+
+for i in range(1, number + 1):
+  
